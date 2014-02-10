37be4b35e352ccedc65611558f1ca9a311fe6e63
Ринат Ибрагимов <XaoZlo@rambler.ru>
2014-02-10 09:16:26 +0600

    countdown block added
    included jQuery 2.0.2 and jQuery.countdown

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-(function(window, document, undefined){
+(function(window, document, $, undefined){
  "use strict";
+ var countdownObj  = {timestamp: (new Date(2014,1,14))}; // 2014 февраль 14
  
+ $(document).ready(function(){
+  $('#countdown').countdown(countdownObj);
+	
 
- window.onload = function(){
-  
- }
-})(window, document);
+ });
+})(window, document, jQuery);
